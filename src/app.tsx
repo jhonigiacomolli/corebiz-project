180ff01f0610578303e2522cac82db58a8f83487
Jhoni <producao@microsite.net.br>
2021-11-23 01:23:03 -0300

    Create fake slider component

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,14 @@
 import { Footer } from "components/footer";
 import { Header } from "components/header";
 import { Newsletter } from "components/newsletter";
+import { Slide } from "components/slide-show/slide";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main>
+        <Slide />
         <Newsletter />
       </main>
       <Footer />
---
+++ src/components/slide-show/slide.tsx
@@ -0,0 +1,26 @@
+import styles from './styles.module.css'
+import { ReactComponent as Cover} from 'assets/banner.svg'
+import image from 'assets/image-2.png'
+
+export const Slide = () => {
+    return (
+        <div className={styles.slide}>
+            <div className={styles.background}>
+                <Cover />
+                <img src={image} alt="slide" />
+            </div>
+            <div className={styles.content}>
+                <div className={styles.centerContent}>
+                    <h1>Olá, o que você está buscando?</h1>
+                    <h2>Criar ou migrar seu e-commerce?</h2>
+                </div>
+            </div>
+            <div className={styles.slider}>
+                <span className={styles.active}></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    )
+}
